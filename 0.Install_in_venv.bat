0ac257c0cfb5a480f33b88653d46f9b61634d9c1
Waves_Man <104572398+WavesMan@users.noreply.github.com>
2024-11-18 15:27:09 +0800

    修改为使用pip清华源

--- 0.Install_in_venv.bat
+++ 0.Install_in_venv.bat
@@ -10,22 +10,22 @@ pause
 echo Entered venv environment
 pause
 echo Installing the dependency packages required for ExVR...
-pip install cv2_enumerate_cameras==1.1.17
-pip install keyboard==0.13.5
-pip install matplotlib==3.9.2
-pip install mediapipe==0.10.13
-pip install numpy==2.1.3
-pip install opencv_contrib_python==4.10.0.84
-pip install opencv_python==4.10.0.84
-pip install pygrabber==0.2
-pip install pynput==1.7.7
-pip install PyQt5==5.15.11
-pip install PyQt5_sip==12.15.0
-pip install python_osc==1.9.0
-pip install scipy==1.14.1
-pip install torch==2.5.0
-pip install torchvision==0.20.1
-pip install msvc-runtime
+pip install cv2_enumerate_cameras==1.1.17 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install keyboard==0.13.5 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install matplotlib==3.9.2 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install mediapipe==0.10.13 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install numpy==2.1.3 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install opencv_contrib_python==4.10.0.84 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install opencv_python==4.10.0.84 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install pygrabber==0.2 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install pynput==1.7.7 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install PyQt5==5.15.11 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install PyQt5_sip==12.15.0 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install python_osc==1.9.0 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install scipy==1.14.1 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install torch==2.5.0 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install torchvision==0.20.1 -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+pip install msvc-runtime -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
 echo Installation complete
 pause
 
